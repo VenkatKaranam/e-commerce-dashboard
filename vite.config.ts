df6f198e3e3d59d5ee7b6a7e28beb6431904557e
Venkat Karanam <karanamvasu871@gmail.com>
2024-08-07 01:32:00 +0530

    githup pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/e-commerce-dashboard",
   plugins: [react()],
   resolve:{
     alias:  [ {find: '@', replacement: path.resolve(__dirname, 'src')}]
